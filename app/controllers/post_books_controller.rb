64201acd8f965eb393294a28c1ca7274a5a91527
Takumi Sato <satotaku.kamigou@gmail.com>
2018-01-07 15:18:25 +0900

    アクションメソッドを追加

---
+++ app/controllers/post_books_controller.rb
@@ -0,0 +1,21 @@
+class PostBooksController < ApplicationController
+
+  # Home画面に移動
+  def home
+  end
+
+  # Users一覧画面に移動
+  def users
+  end
+
+  #投稿の保存
+  def create
+  end
+
+  # books投稿リストの表示画面
+  def index
+  end
+  # 投稿詳細の表示画面
+  def show
+  end
+end
