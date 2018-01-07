12509d1bb2bddc8b5baa3a8f9fa0ad41f00816ea
Takumi Sato <satotaku.kamigou@gmail.com>
2018-01-07 15:28:43 +0900

    ヘッダーの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'post_books#index'
+  resources :post_books, only: [:create,:index,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # deviseではログイン認証が成功した場合、config/routes.rb内で設定されているrootパスへリダイレクトされる。
   # この時点ではrootパスが設定されていないので、デフォルトのYay!You're on Rails!の画面へリダイレクトされる。
